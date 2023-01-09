6cf0cae259abaa0db55ead22b69d4cbf9337d273
Jason <jasonpalacios1346@gmail.com>
2023-01-09 10:49:10 -0600

    Primer commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = ({ target }) => {
+
+        setInputValue(target.value);
+
+    }
+
+    const onSubmit= (event) => {
+        event.preventDefault();
+        const newInputValue = inputValue.trim();
+        if( newInputValue.length <= 1 ) return;
+
+        onNewCategory( newInputValue );
+        setInputValue('');
+        
+    }
+
+  return (
+        <form onSubmit={ onSubmit }>
+            <input 
+                type="text" 
+                placeholder="Buscar gifs"
+                value={ inputValue }
+                onChange={ onInputChange }
+            />
+        </form>
+  )
+}
+
+// AddCategory.propTypes = {
+//     setCategories : PropTypes.func.is
+// }
